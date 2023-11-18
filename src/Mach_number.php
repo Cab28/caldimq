53d8acbf5a47886e3a0fe2652dfd7830d9c0117f
Анатолий <149943532+Cab28@users.noreply.github.com>
2023-11-18 23:21:17 +0300

    Update Mach_number.php
    1. Добавлена переменная для числа Маха
    2. Добавлены геттеры и сеттеры для каждой величины
    3. Зафинален класс
    4. Конструктор теперь пустой
    5. Теперь при делении на ноль выброс исключения

--- src/Mach_number.php
+++ src/Mach_number.php
@@ -1,20 +1,57 @@
 <?php
-
-class Mach_Number
+use DivisionByZeroException;
+final class Mach_Number
 {
     /* Скорость потока */
     protected int|float $v;
     /* Скорость звука */
     protected int|float $c;
+    /* Число Маха */
+    protected int|float $M;
 
-    public function __construct(int|float $v, int|float $c) 
+    public function __construct() 
+    {
+        
+    }
+    
+    public function setV(int|float $v): Mach_Number
     {
         $this->v = $v;
+        return $this;
+    }
+    
+    public function getV()
+    {
+        return $this->v;
+    }
+    
+    public function setC(int|float $c): Mach_Number
+    {
         $this->c = $c;
+        return $this;
+    }
+    
+    public function getC()
+    {
+        return $this->c;
+    }
+    
+    public function setM(int|float $M): Mach_Number
+    {
+        $this->M = $M;
+        return $this;
+    }
+    
+    public function getM()
+    {
+        return $this->M;
     }
     
     public function calc(): int|float
     {
-        return $this->v / $this->v;
+        if (!$this->c) {
+            throw new DivisionByZeroException();
+        }
+        return $this->v / $this->c;
     }
 }
