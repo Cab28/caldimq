5b59d5a8b71fd60959ad0ecb4ca35224302d4edd
Анатолий <149943532+Cab28@users.noreply.github.com>
2023-11-26 13:48:09 +0300

    Update Mach_number.php
    Поправил отступы у комментариев

--- src/Mach_number.php
+++ src/Mach_number.php
@@ -3,12 +3,12 @@ use DivisionByZeroException;
 final class Mach_Number
 {
     /**
-	 * Вычисление числа Маха.
-	 * @param int|float $object_speed
-	 * @param int|float $speed_sound
-	 * @return int|float
-	 * @throws DivisionByZeroExceptio При делении на ноль.
-	 */
+     * Вычисление числа Маха.
+     * @param int|float $object_speed
+     * @param int|float $speed_sound
+     * @return int|float
+     * @throws DivisionByZeroExceptio При делении на ноль.
+     */
     public static function Mach(int|float $object_speed, int|float $speed_sound): int|float
     {
         if (!$speed_sound) {
@@ -18,23 +18,23 @@ final class Mach_Number
     }
 
     /**
-	 * Вычисление скорости объекта.
-	 * @param int|float $speed_sound
-	 * @param int|float $Mach
-	 * @return int|float
-	 */
+     * Вычисление скорости объекта.
+     * @param int|float $speed_sound
+     * @param int|float $Mach
+     * @return int|float
+     */
     public static function ObjectSpeed(int|float $speed_sound, int|float $Mach): int|float
     {
         return $speed_sound * $Mach;
     }
 
     /**
-	 * Вычисление скорости звука в данной среде.
-	 * @param int|float $object_speed
-	 * @param int|float $Mach
-	 * @return int|float
-	 * @throws DivisionByZeroExceptio При делении на ноль.
-	 */
+     * Вычисление скорости звука в данной среде.
+     * @param int|float $object_speed
+     * @param int|float $Mach
+     * @return int|float
+     * @throws DivisionByZeroExceptio При делении на ноль.
+     */
     public static function SpeedSound(int|float $object_speed, int|float $Mach): int|float
     {
         if (!$Mach) {
